c3e05270a4ef73bcdfd8305e7b9eae00cd8a137e
Raviraj Chilka <127545096+ravirajchilka@users.noreply.github.com>
2025-08-06 22:54:38 -0400

    Update add_rigidbody_prims.py

--- Python/isaac-sim-specefic/PhysX/add_rigidbody_prims.py
+++ Python/isaac-sim-specefic/PhysX/add_rigidbody_prims.py
@@ -14,8 +14,31 @@ for path in prim_paths_ar:
         continue
 
     UsdPhysics.RigidBodyAPI.Apply(prim)
-    rigid_body_api = UsdPhysics.RigidBodyAPI.Get(stage,path)
     prim.CreateAttribute("physics:rigidBodyEnabled", Sdf.ValueTypeNames.Bool).Set(True)
     print(f"added rigid body to given prims")
-    if path == "/World/asm/mount":
-        rigid_body_api.CreateKinematicEnabledAttr().Set(True)
+
+    # apply kinematic rigid body
+    UsdPhysics.RigidBodyAPI.Get(stage,"/World/asm/mount").CreateKinematicEnabledAttr().Set(True)
+        
+
+
+# import omni.usd
+# from pxr import UsdPhysics, Sdf
+
+# stage = omni.usd.get_context().get_stage()
+
+# prim_paths_ar = ["/World/asm/disk","/World/asm/mount"]
+
+# for path in prim_paths_ar:
+#     prim = stage.GetPrimAtPath(path)
+
+#     if not prim.IsValid():
+#         print(f"prim is not valid")
+#         continue
+
+#     UsdPhysics.RigidBodyAPI.Apply(prim)
+#     rigid_body_api = UsdPhysics.RigidBodyAPI.Get(stage,path)
+#     prim.CreateAttribute("physics:rigidBodyEnabled", Sdf.ValueTypeNames.Bool).Set(True)
+#     print(f"added rigid body to given prims")
+#     if path == "/World/asm/mount":
+#         rigid_body_api.CreateKinematicEnabledAttr().Set(True)
